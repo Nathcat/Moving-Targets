2c6c884d35270d76f2fa6fb5d826fc8190fa164c
Nathcat <74734054+Nathcat@users.noreply.github.com>
2023-06-05 17:38:27 +0100

    Created Can scene

---
+++ Assets/Scripts/Game/Cans/CanTouchController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanTouchController : MonoBehaviour
+{
+    public float throwForce = 5.0f;
+
+    void Start() {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android) {
+            if (Input.touchCount == 1) {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Ended) {
+                    Vector3 touchPosition = transform.gameObject.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 7f));
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position, touchPosition - transform.position, out hit)) {
+                        if (hit.collider.transform.gameObject.CompareTag("Can")) {
+                            hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * throwForce, ForceMode.Impulse);
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            if (Input.GetMouseButtonUp(0)) {
+                Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 7f);
+                mousePosition = transform.gameObject.GetComponent<Camera>().ScreenToWorldPoint(mousePosition);
+
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, mousePosition - transform.position, out hit)) {
+                    if (hit.collider.transform.gameObject.CompareTag("Can")) {
+                        Debug.Log(hit.collider.gameObject);
+                        hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * throwForce, ForceMode.Impulse);
+                    }
+                }
+            }
+        }
+    }
+}
